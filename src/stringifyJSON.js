ef6714acbedf63c21b43a11d66450723188d305b
Belalfaouri <belalfaourig@gmail.com>
2018-03-10 22:30:56 +0200

    stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,47 @@
 
 // but you don't so you're going to write it from scratch:
 
+
 var stringifyJSON = function(obj) {
-  // your code goes here
-};
+	if(typeof obj === 'number'){
+		return ''+obj;
+	}
+	else if (obj === null){
+		return 'null';
+	} else if (typeof obj === 'boolean'){
+		return ''+obj
+	}
+	else if(Array.isArray(obj)){
+		if(obj.length > 1){
+			var newStr='[';
+			for(var i = 0 ; i<obj.length;i++){
+				if(typeof obj[i] === 'string'){
+					newStr+='"'+obj[i]+'"'+','
+				}else{
+					newStr+=obj[i]+',';
+				}
+			}
+			return newStr.slice(0,-1)+']';
+		}
+		if(obj.length === 0){
+			return '[]';
+		}
+		else if(typeof obj[0] === 'string'){
+		return '['+'"'	+obj +'"'+ ']';
+		}
+		else{
+			return '['+	obj+ ']';
+
+		}
+
+	}
+	else if(typeof obj === 'string'){
+		return '"'+obj+'"';
+	}
+
+
+
+
+	return obj;
+
+}
